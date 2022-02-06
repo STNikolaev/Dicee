e4ca8ad9997dc81092aee6ebff7f42f4ad3cb1b6
STNikolaev <a.o.nikolaev@icloud.com>
2022-02-06 14:30:59 +0300

    ready without constraints

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var rightDiceImageView: UIImageView!
+    @IBOutlet weak var rollButtonView: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        leftDiceImageView.layer.cornerRadius = leftDiceImageView.frame.height / 4
+        rightDiceImageView.layer.cornerRadius = rightDiceImageView.frame.height / 4
+        rollButtonView.layer.cornerRadius = rollButtonView.frame.height / 8
     }
 
-
+    @IBAction func rollButtonDidPress(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        leftDiceImageView.image = diceArray.randomElement()
+        rightDiceImageView.image = diceArray.randomElement()
+    }
 }
 
